3c36fafe176e9422fc12678265b3a7957ee8e910
Yuri Smirnov <tycooon@yandex.ru>
2022-05-24 19:34:44 +0300

    add customizations

--- lib/yabeda/rails.rb
+++ lib/yabeda/rails.rb
@@ -7,10 +7,9 @@ require "yabeda/rails/railtie"
 module Yabeda
   # Minimal set of Rails-specific metrics for using with Yabeda
   module Rails
-    LONG_RUNNING_REQUEST_BUCKETS = [
-      0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, # standard
-      30, 60, 120, 300, 600, # Sometimes requests may be really long-running
-    ].freeze
+    TAGS = %i[controller action status format method].freeze
+    DEFAULT_BUCKETS = [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10].freeze
+    LONG_RUNNING_REQUEST_BUCKETS = [*DEFAULT_BUCKETS, 30, 60, 120, 300, 600].freeze
 
     class << self
       def controller_handlers
@@ -27,45 +26,76 @@ module Yabeda
         Yabeda.configure do
           group :rails
 
-          counter   :requests_total,   comment: "A counter of the total number of HTTP requests rails processed.",
-                                       tags: %i[controller action status format method]
-
-          histogram :request_duration, tags: %i[controller action status format method],
-                                       unit: :seconds,
-                                       buckets: LONG_RUNNING_REQUEST_BUCKETS,
-                                       comment: "A histogram of the response latency."
-
-          histogram :view_runtime, unit: :seconds, buckets: LONG_RUNNING_REQUEST_BUCKETS,
-                                   comment: "A histogram of the view rendering time.",
-                                   tags: %i[controller action status format method]
-
-          histogram :db_runtime, unit: :seconds, buckets: LONG_RUNNING_REQUEST_BUCKETS,
-                                 comment: "A histogram of the activerecord execution time.",
-                                 tags: %i[controller action status format method]
-
-          ActiveSupport::Notifications.subscribe "process_action.action_controller" do |*args|
-            event = ActiveSupport::Notifications::Event.new(*args)
-            labels = {
-              controller: event.payload[:params]["controller"],
-              action: event.payload[:params]["action"],
-              status: event.payload[:status],
-              format: event.payload[:format],
-              method: event.payload[:method].downcase,
-            }
-            labels.merge!(event.payload.slice(*Yabeda.default_tags.keys - labels.keys))
-
-            rails_requests_total.increment(labels)
-            rails_request_duration.measure(labels, Yabeda::Rails.ms2s(event.duration))
-            rails_view_runtime.measure(labels, Yabeda::Rails.ms2s(event.payload[:view_runtime]))
-            rails_db_runtime.measure(labels, Yabeda::Rails.ms2s(event.payload[:db_runtime]))
-
-            Yabeda::Rails.controller_handlers.each do |handler|
-              handler.call(event, labels)
-            end
+          counter :requests_total do
+            comment "A counter of the total number of HTTP requests rails processed."
+            tags %i[controller action status format method]
           end
+
+          histogram :request_duration do
+            comment "A histogram of the response latency."
+            buckets LONG_RUNNING_REQUEST_BUCKETS
+            unit :seconds
+            tags %i[controller action status format method]
+          end
+
+          histogram :view_runtime do
+            comment "A histogram of the view rendering time."
+            buckets LONG_RUNNING_REQUEST_BUCKETS
+            unit :seconds
+            tags %i[controller action status format method]
+          end
+
+          histogram :db_runtime do
+            comment "A histogram of the activerecord execution time."
+            buckets LONG_RUNNING_REQUEST_BUCKETS
+            unit :seconds
+            tags %i[controller action status format method]
+          end
+
+          histogram :cpu_time do
+            comment "A histogram of CPU time."
+            buckets DEFAULT_BUCKETS
+            tags TAGS
+          end
+        end
+
+        subscribe!
+
+        on_controller_action do |event, labels|
+          Yabeda.rails_requests_total.increment(labels)
+          Yabeda.rails_request_duration.measure(labels, ms2s(event.duration))
+          Yabeda.rails_view_runtime.measure(labels, ms2s(event.payload[:view_runtime]))
+          Yabeda.rails_db_runtime.measure(labels, ms2s(event.payload[:db_runtime]))
+          Yabeda.rails_cpu_time.measure(labels, event.cpu_time)
         end
       end
       # rubocop: enable Metrics/MethodLength, Metrics/BlockLength, Metrics/AbcSize
+
+      def subscribe!
+        ActiveSupport::Notifications.subscribe("process_action.action_controller") do |*args|
+          event = ActiveSupport::Notifications::Event.new(*args)
+          process_event!(event)
+        end
+      end
+
+      def process_event!(event)
+        labels = generate_labels(event)
+        labels.merge!(event.payload.slice(*Yabeda.default_tags.keys - labels.keys))
+
+        controller_handlers.each do |handler|
+          handler.call(event, labels)
+        end
+      end
+
+      def generate_labels(event)
+        {
+          controller: event.payload[:params]["controller"],
+          action: event.payload[:params]["action"],
+          status: event.payload[:status],
+          format: event.payload[:format],
+          method: event.payload[:method].downcase,
+        }
+      end
 
       def ms2s(milliseconds)
         (milliseconds.to_f / 1000).round(3)
